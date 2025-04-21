ba6d326a90e9574ae3907a42047032331b213aa7
bschar01 <benjamin.scharr_weiner@tufts.edu>
2025-04-21 09:56:45 -0400

    Add files via upload

---
+++ stocks.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb+srv://bschar01:crosscountry%231@cluster0.k9kkhp0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri);
+
+async function run() {
+
+  try {
+    await client.connect();
+    const database = client.db('Stock');
+    const publicComp = database.collection('Public Companies');
+
+    
+    const fs = require("fs");
+    const { parse } = require("csv-parse");
+
+    const parser = fs.createReadStream("companies-1.csv")
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    for await (const row of parser) {
+      console.log("Inserting this lovely row", row);
+      await publicComp.insertOne({Company: row[0], Ticker: row[1], Price: row[2]})
+
+    }
+    console.log("Yay - inserted");
+
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
