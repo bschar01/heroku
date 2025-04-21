da7e8e7360fbdd708d62e5781b5429db497724d0
bschar01 <benjamin.scharr_weiner@tufts.edu>
2025-04-21 09:55:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve the "index.html" directly from the main folder
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html')); // Change the path as needed
+});
 
+// Listen for incoming requests
 app.listen(port, () => {
-  console.log(`App running on http://localhost:${port}`);
+  console.log(`App running at http://localhost:${port}`);
 });
