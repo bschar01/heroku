08e06ff7d9bc50fa8a4a231d09f38bd4138168dc
bschar01 <benjamin.scharr_weiner@tufts.edu>
2025-04-21 10:09:25 -0400

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -26,8 +26,24 @@ app.get('/submit', (req, res) => {
   res.end();
 
   console.log("Form submitted with id:", id);
-});*/
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+}); */
+
+var http = require('http');
+var url = require('url');
+var port = process.env.PORT || 3000;
+//var port = 8080;   //uncomment to run local
+console.log("This goes to the console window");
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  urlObj = url.parse(req.url,true)
+  id = urlObj.query.id
+   res.write ("Success!  This app is deployed online");
+  res.write("<h2>This is my hello application</h2>");
+  res.write ("The id is: " + id)
+  res.end();
+  console.log('hey')
+}).listen(port);
